1de9683b25e4f16b445c5781b77eb3e48e17552f
matan3857 <matan38355@gmail.com>
2021-11-30 17:32:53 +0200

    workoutTypePreview class to function

--- src/cmps/WorkoutTypePreview.jsx
+++ src/cmps/WorkoutTypePreview.jsx
@@ -1,30 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-import chest from '../assets/img/Chest.png';
+export function WorkoutTypePreview(props) {
 
-export class _WorkoutTypePreview extends Component {
-    state = {
-    }
-    
-    render() {
-        const { exerciseType } = this.props
-        console.log('exerciseType:', exerciseType)
-
-        if (!exerciseType) return <div>Loading...</div>
-
-        return (
-            <div className="exercise-type">
-                <img src={require(`../assets/img/${exerciseType}.png`).default} />
-            </div>
-        )
-    }
+    const { exerciseType } = props
+    return (
+        <div className="exercise-type">
+            <img src={require(`../assets/img/${exerciseType}.png`).default} />
+        </div>
+    )
 }
-
-function mapStateToProps(state) {
-    return {
-    };
-}
-
-export const WorkoutTypePreview = connect(mapStateToProps)(_WorkoutTypePreview);
