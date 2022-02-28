137595110c42a1753ab2a57bf54508c42bdd0947
matan3857 <matan38355@gmail.com>
2022-02-28 21:59:18 +0200

    check

--- src/cmps/LoaderSpinner.jsx
+++ src/cmps/LoaderSpinner.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import Loader from "react-loader-spinner";
 
 export  function LoaderSpinner() {
@@ -7,10 +6,8 @@ export  function LoaderSpinner() {
         <Loader
         type="TailSpin"
         color="#302f2f"
-        // color="#00BFFF"
         height={90}
         width={90}
-        // timeout={3000} 
       />
     )
 }
