a21f8375e5a9482eaba4574f5b8f8627636dc2ec
matan3857 <matan38355@gmail.com>
2022-03-13 13:41:43 +0200

    update toggle sets & done workout

--- src/pages/StartWorkout.jsx
+++ src/pages/StartWorkout.jsx
@@ -2,8 +2,7 @@ import React, { useState } from 'react';
 import { connect } from "react-redux";
 import { Timer } from '../cmps/StartWorkout/Timer'
 import { LoaderSpinner } from '../cmps/LoaderSpinner'
-
-
+import { Link } from "react-router-dom";
 
 function _StartWorkout(props) {
     const { user } = props
@@ -17,21 +16,22 @@ function _StartWorkout(props) {
         const reversedIdx = reverseLink.substring(0, stopIdx)
         const currectIdx = reversedIdx.split("").reverse().join("")
 
-        console.log('currectIdx', currectIdx)
-        console.log('user', user)
+        // console.log('currectIdx', currectIdx)
+        // console.log('user', user)
         workoutToDo = user.workouts[currectIdx]
-        console.log('workoutToDo', workoutToDo)
+        // console.log('workoutToDo', workoutToDo)
     }
-
-    const [setsCount, setSetsCount] = useState(0);
     const [currExerciseIdx, setCurrExerciseIdx] = useState(0);
-    const [loaded, setLoaded] = useState(false);
 
     const exercise = workoutToDo.ex[currExerciseIdx]
+    const [setsCount, setSetsCount] = useState(+exercise.sets);
+    const [loaded, setLoaded] = useState(false);
+    const [isInfo, setIsInfo] = useState(false);
+    const [isDoneWorkout, setIsDoneWorkout] = useState(false);
+
     console.log('Exercise', exercise)
 
     const onToggleExercise = (isNext) => {
-
         if (isNext) {
             if (currExerciseIdx + 1 > workoutToDo.ex.length - 1) return
             setCurrExerciseIdx(currExerciseIdx + 1)
@@ -44,27 +44,74 @@ function _StartWorkout(props) {
         }
     }
 
+    const onToggleInfo = () => {
+        setIsInfo(prevIsInfo => !prevIsInfo)
+    }
+
+    const onNextRep = () => {
+        if (setsCount - 1 === 0) {
+            //Check if last exercise
+            if (currExerciseIdx === workoutToDo.ex.length - 1) setIsDoneWorkout(true)
+            else {
+                setSetsCount(+exercise.sets)
+                onToggleExercise(1)
+            }
+        }
+        else {
+            setSetsCount(prevSetsCount => prevSetsCount - 1)
+        }
+    }
+
     return (
         <div className="start-workout margin-top">
             <h1>Starting workout: {workoutToDo.workoutTitle}</h1>
             <Timer />
             {exercise ?
                 <div className='start-workout-exercises'>
-                    <button onClick={() => { onToggleExercise(0) }}>Prev Exercise</button>
-                    <button onClick={() => { onToggleExercise(1) }}>Next Exercise</button>
+                    {isDoneWorkout
+                        ?
+                        <>
+                            <p className='done-workout'>Well done, You've finished your workout!</p>
+                            <Link to={{ pathname: '/menu' }} >
+                                <button className='light-btn'>Back to Application</button>
+                            </Link>
+                        </>
+                        :
+                        <>
+                            <div className='toggle-btns'>
+                                <button className='light-btn' onClick={() => { onToggleExercise(0) }}>Previous Exercise</button>
+                                <button className='light-btn' onClick={() => { onToggleExercise(1) }}>Next Exercise</button>
+                            </div>
 
-                    <div className='exercise-info'>
-                        <h2>Now doing: {exercise.title}</h2>
-                        <div
-                            className='imgs-container flex justify-center'
-                            style={{ display: loaded ? 'block' : 'none' }}
-                            onLoad={() => setLoaded(true)}
-                        >
-                            <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img1}.png`).default} alt="exercise_1" />
-                            <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img2}.png`).default} alt="exercise_2" />
-                        </div>
-                        {!loaded && <LoaderSpinner /> }
-                    </div>
+                            <div className='exercise-info'>
+                                <h2>Now doing: {exercise.title}</h2>
+                                <span>Remaining Sets: {setsCount}</span>
+                                <span>Reps Per Set: {exercise.reps}</span>
+                                {/* {
+                            Array.apply(null, { length: +exercise.sets }).map((e, i) => (
+                                <span className="busterCards" key={i}>
+                                    ♦
+                                </span>
+                            ))
+                        } */}
+                                <br />
+                                <button className='done-rep-btn' onClick={onNextRep}>Next Rep!</button>
+                                {isInfo &&
+                                    <p>{exercise.desc}</p>
+                                }
+                                <div
+                                    className='imgs-container flex justify-center'
+                                    style={{ display: loaded ? 'block' : 'none' }}
+                                    onLoad={() => setLoaded(true)}
+                                >
+                                    <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img1}.png`).default} alt="exercise_1" />
+                                    <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img2}.png`).default} alt="exercise_2" />
+                                </div>
+                                {!loaded && <LoaderSpinner />}
+                                <button className='light-btn light-btn-info' onClick={onToggleInfo}>{isInfo ? 'Close..' : 'More Info?'}</button>
+                            </div>
+                        </>
+                    }
                 </div>
                 :
                 <LoaderSpinner />
