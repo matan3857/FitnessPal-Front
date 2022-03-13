edd079da36f78e870a7fa81f2207863d84a20350
matan3857 <matan38355@gmail.com>
2022-03-13 12:02:00 +0200

    update images loading & toggle

--- src/pages/StartWorkout.jsx
+++ src/pages/StartWorkout.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 import { Timer } from '../cmps/StartWorkout/Timer'
+import { LoaderSpinner } from '../cmps/LoaderSpinner'
+
 
 
 function _StartWorkout(props) {
@@ -20,10 +22,54 @@ function _StartWorkout(props) {
         workoutToDo = user.workouts[currectIdx]
         console.log('workoutToDo', workoutToDo)
     }
+
+    const [setsCount, setSetsCount] = useState(0);
+    const [currExerciseIdx, setCurrExerciseIdx] = useState(0);
+    const [loaded, setLoaded] = useState(false);
+
+    const exercise = workoutToDo.ex[currExerciseIdx]
+    console.log('Exercise', exercise)
+
+    const onToggleExercise = (isNext) => {
+
+        if (isNext) {
+            if (currExerciseIdx + 1 > workoutToDo.ex.length - 1) return
+            setCurrExerciseIdx(currExerciseIdx + 1)
+            setLoaded(false)
+        }
+        else {
+            if (currExerciseIdx - 1 < 0) return
+            setCurrExerciseIdx(currExerciseIdx - 1)
+            setLoaded(false)
+        }
+    }
+
     return (
         <div className="start-workout margin-top">
             <h1>Starting workout: {workoutToDo.workoutTitle}</h1>
-            <Timer/>
+            <Timer />
+            {exercise ?
+                <div className='start-workout-exercises'>
+                    <button onClick={() => { onToggleExercise(0) }}>Prev Exercise</button>
+                    <button onClick={() => { onToggleExercise(1) }}>Next Exercise</button>
+
+                    <div className='exercise-info'>
+                        <h2>Now doing: {exercise.title}</h2>
+                        <div
+                            className='imgs-container flex justify-center'
+                            style={{ display: loaded ? 'block' : 'none' }}
+                            onLoad={() => setLoaded(true)}
+                        >
+                            <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img1}.png`).default} alt="exercise_1" />
+                            <img src={require(`../assets/img/${exercise.type}/${exercise.title}/${exercise.img2}.png`).default} alt="exercise_2" />
+                        </div>
+                        {!loaded && <LoaderSpinner /> }
+                    </div>
+                </div>
+                :
+                <LoaderSpinner />
+            }
+
         </div>
     )
 }
