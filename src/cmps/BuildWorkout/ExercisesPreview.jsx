1b7e309f9fe715a2efb959058aaa2132b3b3cab8
matan3857 <matan38355@gmail.com>
2022-03-16 10:28:01 +0200

    loading exercises img

--- src/cmps/BuildWorkout/ExercisesPreview.jsx
+++ src/cmps/BuildWorkout/ExercisesPreview.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 import { LoaderSpinner } from '../LoaderSpinner'
 
 export function _ExercisesPreview(props) {
   const { exercise, exerciseType, onAddExerciseToWorkout, onShowExerciseDetails, user } = props
+  const [loaded, setLoaded] = useState(false);
+
   if (!exercise) return <LoaderSpinner />
   return (
     <div className="exercise-preview">
       <h1>{exercise.title}</h1>
       {exercise.desc.length > 60 ? <h3>{`${exercise.desc.slice(0, 60)}...`}</h3> : <h3>{exercise.desc}</h3>}
-      <img src={require(`../../assets/img/${exerciseType}/${exercise.title}/${exercise.img1}.png`).default} alt="exercise"/>
+      <img
+        style={{ display: loaded ? 'block' : 'none' }}
+        onLoad={() => setLoaded(true)}
+        src={require(`../../assets/img/${exerciseType}/${exercise.title}/${exercise.img1}.png`).default} alt="exercise"
+      />
+      {!loaded && <LoaderSpinner />}
 
       <div className="exercise-btns flex column">
         <button className="exercise-btn" onClick={() => { onAddExerciseToWorkout(exercise) }}>Add to Workout list</button>
         <button className="exercise-btn" onClick={() => { onShowExerciseDetails(exercise) }}>More Details...</button>
-        {user && user.isAdmin && <button onClick={() => { onShowExerciseDetails(exercise , true) }} className="exercise-btn">Edit</button>}
+        {user && user.isAdmin && <button onClick={() => { onShowExerciseDetails(exercise, true) }} className="exercise-btn">Edit</button>}
       </div>
     </div>
   )
