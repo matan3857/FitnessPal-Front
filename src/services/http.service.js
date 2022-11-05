b191f392d0e56187bc3a194c8e54b71d1d3f4bc8
Matan Levi <matan38355@gmail.com>
2022-11-05 21:22:13 +0200

    added backend url - Render

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -3,8 +3,8 @@ import Axios from 'axios'
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? 'https://fitnesspal-backend.onrender.com/api/'
     // ? '/api/'
-    // : 'https://fitnesspal-backend.onrender.com/api/'
-    : '//localhost:3030/api/'
+    : 'https://fitnesspal-backend.onrender.com/api/'
+    // : '//localhost:3030/api/'
 
 
 var axios = Axios.create({
